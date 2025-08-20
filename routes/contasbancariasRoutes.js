81103b82e3628aad43190becbb65d37737d6a436
martorelecalixto <martorele@gmail.com>
2025-08-20 19:40:44 -0300

    alterações genericas + titulosreceber + titulospagar 202508201940

--- routes/contasbancariasRoutes.js
+++ routes/contasbancariasRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contasbancariasController = require('../controllers/contasBancariasController');
+const contasbancariasController = require('../controllers/contasbancariasController');
 
 router.get('/', contasbancariasController.getContasBancarias);
 
